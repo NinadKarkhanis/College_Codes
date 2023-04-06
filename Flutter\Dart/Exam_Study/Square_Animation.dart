ce62ac21fb81b705a17f7506bb10b60e1146ae7d
Ninad <95896935+Nnknoob@users.noreply.github.com>
2023-04-06 08:03:34 +0530

    Update Square_Animation.dart

--- Flutter\Dart/Exam_Study/Square_Animation.dart
+++ Flutter\Dart/Exam_Study/Square_Animation.dart
@@ -1,67 +1,69 @@
 import 'package:flutter/material.dart';
 
-
 void main() => runApp(MyApp());
-const img_url =
-    'https://www.pngkey.com/png/detail/61-612294_anime-manga-animeboy-one-piece-luffy-manga-anime.png';
-
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  // This widget is the root of your application.
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "hello",
-      theme: ThemeData(brightness: Brightness.dark),
-      home: Myhome(),
+      title: 'Title',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
     );
   }
 }
 
-
-class Myhome extends StatefulWidget {
-  Myhomestate createState() => Myhomestate();
+class MyHomePage extends StatefulWidget {
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+  // This class is the configuration for the state.
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
 }
 
+class _MyHomePageState extends State<MyHomePage> {
+  double _height = 200;
+  double _weight = 200;
+  Color _color = Colors.red;
 
-double _height = 100;
-double _width = 100;
-Color _color = Colors.red;
-
-
-class Myhomestate extends State<Myhome> {
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        children: [
-          AnimatedContainer(
-            duration: Duration(seconds: 4),
-            height: _height,
-            width: _width,
-            color: _color,
-          )
-        ],
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: update,
-        child: Icon(Icons.play_arrow),
-      ),
-    );
-  }
-
-
-  void update() {
+  void animate() {
     setState(() {
       if (_color == Colors.red) {
-        _height = 200;
-        _width = 200;
-        _color = Colors.blue;
+        _height = 400;
+        _weight = 450;
+        _color = Colors.black;
       } else {
-        _height = 100;
-        _width = 100;
+        _height = 200;
+        _weight = 200;
         _color = Colors.red;
       }
     });
   }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          // Here we take the value from the MyHomePage object that was created by
+          // the App.build method, and use it to set our appbar title.
+          title: Text('Square Animation'),
+        ),
+        body: Container(
+          padding: EdgeInsets.all(15),
+          child: Column(children: <Widget>[
+            AnimatedContainer(
+              duration: Duration(seconds: 4),
+              height: _height,
+              width: _weight,
+              color: _color,
+            ),
+            ElevatedButton(onPressed: animate, child: const Text("Press"))
+          ]),
+        ));
+  }
 }
-
-
-
