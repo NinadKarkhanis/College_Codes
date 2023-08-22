f862ac06a1884cb7965029f6420896726e2415a9
Ninad <95896935+Nnknoob@users.noreply.github.com>
2023-08-22 21:02:19 +0530

    Update App.js

--- Sem5/Mern/Prac6/3/App.js
+++ Sem5/Mern/Prac6/3/App.js
@@ -1,1 +1,35 @@
+import React from "react";
+import Student from "./stud";
+
+function App() {
+  return (
+    <>
+      <h1 style={{color:"red", textAlign:"center"}}>Student Data</h1>
+      <table border="1px" style={{margin:'0 auto'}}>
+        <tr>
+        <th>Student ID</th>
+        <th>Student Name</th>
+        <th>Student Contact</th>
+        </tr>
+        <tr>
+        <Student id="1" name="Ninad Karkhanis"  contact="13213123" />
+        </tr>
+        <tr>
+        <Student id="2" name="Hamza Kapasi"  contact="13223123" />
+        </tr>
+        <tr>
+        <Student id="3" name="Shawn Wadhwa"  contact="13213173" />
+        </tr>
+        <tr>
+        <Student id="4" name="Tejas Vartak"  contact="13213179" />
+        </tr>
+        <tr>
+        <Student id="5" name="XYZ BAC"  contact="132133279" />
+        </tr>
+      </table>
+    </>
+  );
+}
+
+export default App;
 
