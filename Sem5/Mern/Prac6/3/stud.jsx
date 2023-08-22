4fecda9d27ad00eb033aa7243a2b7557698a727d
Ninad <95896935+Nnknoob@users.noreply.github.com>
2023-08-22 21:02:54 +0530

    Create stud.jsx

---
+++ Sem5/Mern/Prac6/3/stud.jsx
@@ -0,0 +1,12 @@
+const Student=(props)=>{
+    return(
+        <>
+        <td>{props.id}</td>
+        <td>{props.name}</td>
+        <td>{props.contact}</td>
+
+        </>
+    )
+}
+
+export default Student;
