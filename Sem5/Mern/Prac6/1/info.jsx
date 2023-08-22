bee69b0af91745d73ca9070171b19532fb455add
Ninad <95896935+Nnknoob@users.noreply.github.com>
2023-08-22 12:22:15 +0530

    Create info.jsx
    1.Write a program in react to create a functional component for my info that uses h1 tag that display your name red in color with center align. Also display the list of top 3 vacation spots in order list format.
    Use either internal or external css

---
+++ Sem5/Mern/Prac6/1/info.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+
+const MyInfo=()=>{
+  return (
+    <div >
+      <h1 className="name"  style={{textAlign:'center',color:'red'}}>Ninad</h1>
+      <p >Here are the top 3 vacation spots:</p>
+      <ol style={{color:'blue'}}>
+        <li>Destination 1</li>
+        <li>Destination 2</li>
+        <li>Destination 3</li>
+      </ol>
+    </div>
+  );
+}
+
+
+export default MyInfo;
